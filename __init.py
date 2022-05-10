4a8cb5b4f093edf4f0f1328e84242f178fc70310
puseyr <pusey.ronald@gmail.com>
2022-05-10 18:38:34 -0400

    updated init and added phrasing.txt

--- __init.py
+++ __init.py
@@ -4,7 +4,21 @@
 # Create Date: 5/9/2022
 # Version: 0.0.1 [Main Release.Minor Release.BugFix]
 
-word1 = 'where'
-word2 = 'Carmen San Diego'
+import random
+import os
 
-print("Hey Siri " + word1 + " is " + word2 + "?")
+list = open("./english-words-master/words.txt").read()
+line = list[0:]
+
+words = line.split()
+randoWord = random.choice(words)
+
+questionList = open("./english-words-master/phrasing.txt")
+
+
+print(randoWord)
+
+#word1 = 'where'
+#word2 = 'Carmen San Diego'
+
+#print("Hey Siri " + word1 + " is " + word2 + "?")
