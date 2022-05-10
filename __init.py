89fe4ef3299716e9aa13b1561d8c524b28cdabd1
puseyr <pusey.ronald@gmail.com>
2022-05-10 13:49:04 -0400

    first commit

---
+++ __init.py
@@ -0,0 +1,10 @@
+# App: yoSiri
+# Description: A bot to create randomized questions to ask Siri
+# Author: Ronald Pusey
+# Create Date: 5/9/2022
+# Version: 0.0.1 [Main Release.Minor Release.BugFix]
+
+word1 = 'where'
+word2 = 'Carmen San Diego'
+
+print("Hey Siri " + word1 + " is " + word2 + "?")
